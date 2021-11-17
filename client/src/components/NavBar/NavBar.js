09c4e61ffa88c84361b1dc989135a0f47364ba65
Jose Sanchez <sanchezjoserafael25@gmail.com>
2021-11-17 00:17:30 -0400

    Cambiado de Css a Sass

--- client/src/components/NavBar/NavBar.js
+++ client/src/components/NavBar/NavBar.js
@@ -1,19 +1,49 @@
 import React from "react";
-import { Link } from "react-router-dom";
-import { Outlet } from "react-router-dom";
+import { NavLink, Link } from "react-router-dom";
+import styles from "./Navbar.module.scss";
+import Logo from "../../images/dog.png";
+import { BiHome, BiPlusCircle } from "react-icons/bi";
+import Button from "../Button/Button";
+
+const links = [
+  {
+    icon: BiHome,
+    to: "home",
+    text: "Home",
+  },
+];
 
 const Navbar = () => {
   return (
     <>
-      <ul>
-        <li>
-          <Link to="home">Home</Link>
-        </li>
-        <li>
-          <Link to="create">Create</Link>
-        </li>
-      </ul>
-      <Outlet />
+      <nav className={styles.navbar}>
+        <ul className={styles.navbarList}>
+          <li className={styles.logoContainer}>
+            <Link to="/">
+              <img className={styles.logo} src={Logo} alt="Logo" />
+            </Link>
+          </li>
+          {links.map((link) => (
+            <li key={link.text}>
+              <NavLink
+                className={({ isActive }) =>
+                  `${styles.navbarLink} ` + (isActive ? styles.active : "")
+                }
+                to={link.to}
+              >
+                {link.icon ? <link.icon /> : null}
+                {link.text}
+              </NavLink>
+            </li>
+          ))}
+          <Link to="create" className={styles.navbarLinkButton}>
+            <Button>
+              <BiPlusCircle />
+              Create
+            </Button>
+          </Link>
+        </ul>
+      </nav>
     </>
   );
 };
