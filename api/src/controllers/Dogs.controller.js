5e392fbdf8c5c4dd078979f4be0d73ba423c9b78
Jose Sanchez <sanchezjoserafael25@gmail.com>
2021-11-25 19:45:03 -0400

    Solo añadido el next() cuando haya un error y llame al endware que handlea errores de tipo 500

--- api/src/controllers/Dogs.controller.js
+++ api/src/controllers/Dogs.controller.js
@@ -6,98 +6,106 @@ const axios = require("axios");
 
 const NO_RESULTS_MESSAGE = "No Results";
 
-const getDogs = async (req, res) => {
-  const { name } = req.query;
-  const queryAll = breedsQ;
-  const condition = name
-    ? {
-      where: {
-        name: {
-          [Op.substring]: name,
+const getDogs = async (req, res, next) => {
+  try {
+    const { name } = req.query;
+    const queryAll = breedsQ;
+    const condition = name
+      ? {
+        where: {
+          name: {
+            [Op.substring]: name,
+          },
         },
-      },
-    }
-    : {};
+      }
+      : {};
 
-  condition.attributes = ["id", "name", "image", "weight", "height"];
-  condition.include = {
-    model: Temperament,
-    attributes: ["name"],
-    through: { attributes: [] },
-  };
-
-  let breedsDB = await Dog.findAll(condition);
-  const { data: allBreedsApi } = await axios.get(queryAll);
-
-  //INFO: This part is to join the temperaments of the database to only a string temperament. This is only to match the data like the API.
-  // The JSON.parse(JSON.stringify) is to can use the map,it doesn't work without it
-  breedsDB = joinTemperaments(breedsDB);
-
-  // INFO: This is to get the data by name, the API of dogs searing by namedoesn't return the images, so I prefered this option
-  const breeds = name
-    ? allBreedsApi.filter((breed) => {
-      const regex = new RegExp(name, "i");
-      return regex.test(breed.name);
-    })
-    : allBreedsApi;
-
-  let responseBreeds = breeds.map((breed) => {
-    const {
-      id,
-      name,
-      image: { url: image },
-      temperament,
-      weight: { imperial: weight },
-      height: { imperial: height },
-    } = breed;
-
-    return { id, name, image, temperament, weight, height };
-  });
-
-  breedsDB?.length && (responseBreeds = [...breedsDB, ...responseBreeds]);
-  return responseBreeds?.length
-    ? res.json(responseBreeds)
-    : res.status(302).json({ message: NO_RESULTS_MESSAGE });
-};
-
-const getDogById = async (req, res) => {
-  const { idRaza } = req.params;
-  let query = breedsQ;
-
-  const breedDB =
-    isUUID(idRaza) &&
-    (await Dog.findByPk(idRaza, {
-      attributes: ["id", "name", "image", "weight", "height", "life_span"],
-      include: {
-        model: Temperament,
-        attributes: ["name"],
-        through: { attributes: [] },
-      },
-    }));
-
-  //INFO: if the data is in the database, then return it. If not then use the API
-  if (breedDB) return res.json(breedDB);
-
-  const { data: breedsApi } = await axios.get(query);
-  const breed = breedsApi.find((b) => b.id === Number(idRaza));
-
-  if (breed) {
-    const responseObj = {
-      id: breed.id,
-      name: breed.name,
-      image: breed.image.url,
-      temperament: breed.temperament,
-      weight: breed.weight.metric,
-      height: breed.height.metric,
-      life_span: breed.life_span,
+    condition.attributes = ["id", "name", "image", "weight", "height"];
+    condition.include = {
+      model: Temperament,
+      attributes: ["name"],
+      through: { attributes: [] },
     };
-    return res.json(responseObj);
-  }
 
-  return res.status(302).json({ message: NO_RESULTS_MESSAGE });
+    let breedsDB = await Dog.findAll(condition);
+    const { data: allBreedsApi } = await axios.get(queryAll);
+
+    //INFO: This part is to join the temperaments of the database to only a string temperament. This is only to match the data like the API.
+    // The JSON.parse(JSON.stringify) is to can use the map,it doesn't work without it
+    breedsDB = joinTemperaments(breedsDB);
+
+    // INFO: This is to get the data by name, the API of dogs searing by namedoesn't return the images, so I prefered this option
+    const breeds = name
+      ? allBreedsApi.filter((breed) => {
+        const regex = new RegExp(name, "i");
+        return regex.test(breed.name);
+      })
+      : allBreedsApi;
+
+    let responseBreeds = breeds.map((breed) => {
+      const {
+        id,
+        name,
+        image: { url: image },
+        temperament,
+        weight: { imperial: weight },
+        height: { imperial: height },
+      } = breed;
+
+      return { id, name, image, temperament, weight, height };
+    });
+
+    breedsDB?.length && (responseBreeds = [...breedsDB, ...responseBreeds]);
+    return responseBreeds?.length
+      ? res.json(responseBreeds)
+      : res.status(302).json({ message: NO_RESULTS_MESSAGE });
+  } catch (e) {
+    next(e);
+  }
 };
 
-const createDog = async (req, res) => {
+const getDogById = async (req, res, next) => {
+  try {
+    const { idRaza } = req.params;
+    let query = breedsQ;
+
+    const breedDB =
+      isUUID(idRaza) &&
+      (await Dog.findByPk(idRaza, {
+        attributes: ["id", "name", "image", "weight", "height", "life_span"],
+        include: {
+          model: Temperament,
+          attributes: ["name"],
+          through: { attributes: [] },
+        },
+      }));
+
+    //INFO: if the data is in the database, then return it. If not then use the API
+    if (breedDB) return res.json(breedDB);
+
+    const { data: breedsApi } = await axios.get(query);
+    const breed = breedsApi.find((b) => b.id === Number(idRaza));
+
+    if (breed) {
+      const responseObj = {
+        id: breed.id,
+        name: breed.name,
+        image: breed.image.url,
+        temperament: breed.temperament,
+        weight: breed.weight.metric,
+        height: breed.height.metric,
+        life_span: breed.life_span,
+      };
+      return res.json(responseObj);
+    }
+
+    return res.status(302).json({ message: NO_RESULTS_MESSAGE });
+  } catch (e) {
+    next(e);
+  }
+};
+
+const createDog = async (req, res, next) => {
   try {
     const {
       name,
@@ -123,18 +131,18 @@ const createDog = async (req, res) => {
     // CHeck that the min values are not higher that the max values
     if (min_height > max_height) {
       return res.status(403).json({
-        message: 'The min height cant be higher that max height'
-      })
+        message: "The min height cant be higher that max height",
+      });
     }
     if (min_weight > max_weight) {
       return res.status(403).json({
-        message: "The min weight cant be higher that max weight"
-      })
+        message: "The min weight cant be higher that max weight",
+      });
     }
     if (min_years && max_years && min_years > max_years) {
       return res.status(403).json({
         message: "The min years cant be higher that max years",
-      })
+      });
     }
 
     const lifeSpanY =
@@ -164,10 +172,11 @@ const createDog = async (req, res) => {
 
       return res.status(201).json({ message: "Data saved sucessfully" });
     }
-    return res.status(303).json({ message: "There is already a breed with that name" });
+    return res
+      .status(303)
+      .json({ message: "There is already a breed with that name" });
   } catch (e) {
-    res.status(500).json({ message: "An error with the server has ocurred" });
-    console.error(e);
+    next(e);
   }
 };
 
--- api/src/controllers/Temperament.controller.js
+++ api/src/controllers/Temperament.controller.js
@@ -3,31 +3,35 @@ const { breedsQ } = require("../../data/api");
 const { Temperament } = require("../db");
 const { getSeparatedTemps, removeRepeated } = require("../helpers/index");
 
-const getTemperaments = async (_req, res) => {
-  const temperaments = await Temperament.findAll({
-    attributes: ["id", "name"],
-  });
+const getTemperaments = async (_req, res, next) => {
+  try {
+    const temperaments = await Temperament.findAll({
+      attributes: ["id", "name"],
+    });
 
-  // if there is not temperaments in the database, the use the api to get the temperaments, return it to the client and save it in the database for the next use.
-  if (!temperaments.length) {
-    let query = breedsQ;
-    const { data: breedsApi } = await axios.get(query);
+    // if there is not temperaments in the database, the use the api to get the temperaments, return it to the client and save it in the database for the next use.
+    if (!temperaments.length) {
+      let query = breedsQ;
+      const { data: breedsApi } = await axios.get(query);
 
-    let newTemperaments = getSeparatedTemps(breedsApi);
+      let newTemperaments = getSeparatedTemps(breedsApi);
 
-    newTemperaments = removeRepeated(newTemperaments);
-    // convert in a object to can save them in the database
-    newTemperaments = newTemperaments.map((temp, i) => ({
-      id: i + 1,
-      name: temp,
-    }));
-    res.json(newTemperaments);
-    // Save the new Temperaments from the api
-    await Temperament.bulkCreate(newTemperaments);
-    return;
+      newTemperaments = removeRepeated(newTemperaments);
+      // convert in a object to can save them in the database
+      newTemperaments = newTemperaments.map((temp, i) => ({
+        id: i + 1,
+        name: temp,
+      }));
+      res.json(newTemperaments);
+      // Save the new Temperaments from the api
+      await Temperament.bulkCreate(newTemperaments);
+      return;
+    }
+    // else return the temperaments of the database
+    return res.json(temperaments);
+  } catch (e) {
+    next(e);
   }
-  // else return the temperaments of the database
-  return res.json(temperaments);
 };
 
 module.exports = { getTemperaments };
